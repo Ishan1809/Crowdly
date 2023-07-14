3a17e7904d5a4110c65d23ee56efa44ac44fbdfe
Ishan <ishan180901@gmail.com>
2023-07-14 13:07:03 +0530

    modified

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import { Person } from "@mui/icons-material";
+
 
 export default function topbar() {
   return (
-    <div>
-      Hi
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Social</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchBar">
+            <SearchIcon />
+            <input placeholder="Serach for Friends, Posts or Videos" className="searchInput" />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+            <div className="topbarIconItem">
+                <PersonIcon />
+                <span className="topbarIconBadge">1</span>
+            </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
